dd36dc4370b6f568c91569cc5a817ba0642d04d2
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-11-01 22:13:36 -0800

    added app description

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
     <div className="App">    
       <header className="App-header">
         <h1>Welcome to organelle!</h1>
+        <p>
+          Organelle helps your time management by integrating all <br/>
+          the course information in one App, and remind you when <br/>
+          due is immiment.
+        </p>
         <a 
           className="App-link"
           href="https://drive.google.com/drive/folders/1f90RqdXNojKlN-CAytjrkiAQ4xdT5yzP?usp=sharing">
